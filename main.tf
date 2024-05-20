4f5452ed7a2ed8b1be9015725af89dea716b5b41
takakuda <kei.takakuda.0415@icloud.com>
2024-05-20 19:21:56 +0900

    module

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-variable "env" {}
+module "dev_server" {
+  source        = "./http_server"
+  instance_type = "t3.micro"
+}
 
-resource "aws_instance" "example" {
-  ami           = ""
-  instance_type = var.env == "prod" ? "m5.large" : "t3.micro"
+output "public_dns" {
+  value = module.dev_server.public_dns
 }
